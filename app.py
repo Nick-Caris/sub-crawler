3e7108b975a8669e825e21c9218174953d55de3d
Marijn Besseling <ME.Besseling@student.han.nl>
2019-05-11 14:09:13 +0200

    added docstring to confusing method

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import datetime
 
 app = Flask(__name__)
 
+
 @app.route("/subs")
 def subs():
-    return jsonify(crawl(datetime.datetime.now().isocalendar()[1]))
+    return jsonify(crawl(get_week_nr()))
+
+
+def get_week_nr():
+    """Uses the datetime module to grab the current date, get the iso calendar in (ISO year, ISO week number,
+    ISO weekday) format and returns the week number."""
+    return datetime.datetime.now().isocalendar()[1]
